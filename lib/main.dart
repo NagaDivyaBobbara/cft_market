85351edfdfea8f36d0de269b8f0c259e509659fb
Naga Divya Bobbara <nagadivyabobbara@gmail.com>
2024-09-01 18:03:10 +0530

    feat: added firebase configuration for android

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cft_market/pages/sale_order_detail/sale_order_detail_screen.dart
 import 'package:cft_market/serviceLocator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   await initServiceProvider();
--- lib/serviceLocator.dart
+++ lib/serviceLocator.dart
@@ -7,13 +7,18 @@ import 'package:cft_market/pages/my_account/my_account_viewmodel.dart';
 import 'package:cft_market/pages/sale_order_list/sale_order_list_viewmodel.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 
 final serviceLocator = GetIt.instance;
 const jwt = "";
 
 Future<void> initServiceProvider() async {
   await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   serviceLocator.registerLazySingleton<HomeScreenViewmodel>(()=> HomeScreenViewmodel());
   serviceLocator.registerLazySingleton<SaleOrderListApi>(() => SaleOrderListApiImpl());
   serviceLocator.registerLazySingleton<SaleOrderListViewmodel>(() => SaleOrderListViewmodel(saleOrderListApi: serviceLocator.get<SaleOrderListApi>()));
